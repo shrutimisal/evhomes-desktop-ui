e1cec70fe7aa2453de699bd2c444501aeb276762
shruti misal <shrutimisal109@gmail.com>
2025-04-02 17:56:18 +0530

    task cards

--- app/homepage/page.js
+++ app/homepage/page.js
@@ -6,6 +6,7 @@ import Quickaccessoptions from "@/components/QuickAccess/Quickaccessoptions";
 import styles from "./homepage.module.css";
 import React from "react";
 import Extraoptions from "@/components/ExtraOptions/Extraoptions";
+import Taskcards from "@/components/TaskCards/Taskcards";
 
 const HomePage = () => {
   return <div>
@@ -16,6 +17,7 @@ const HomePage = () => {
     <Linegraphcard/>
     <Piegraphcard/>
     </div>
+    <Taskcards/>
   </div>;
 };
 
---
+++ components/TaskCards/Taskcards.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from "./taskcards.module.css";
+
+const Taskcards = () => {
+  const tasks = [
+    { name: "Client Name", remark: "Remark……!" }, 
+    { name: "Client Name", remark: "Remark……!" }, 
+    { name: "Client Name", remark: "Remark……!" }, 
+    { name: "Client Name", remark: "Remark……!" }, 
+    { name: "Client Name", remark: "Remark……!" }, 
+    { name: "Client Name", remark: "Remark……!" }, 
+    { name: "Client Name", remark: "Remark……!" }, 
+    { name: "Client Name", remark: "Remark……!" }, 
+  ];
+
+  return (
+    <div className={styles.taskContainer}>
+      <h2 className={styles.heading}>Tasks</h2>
+      <div className={styles.taskList}>
+        {tasks.map((task, index) => (
+          <div key={index} className={styles.taskCard}>
+            <img src="/profile.jpg" alt="Profile" className={styles.profilePic} />
+            <div className={styles.taskInfo}>
+              <p className={styles.clientName}>{task.name}</p>
+              <p className={styles.remark}>{task.remark}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Taskcards;
