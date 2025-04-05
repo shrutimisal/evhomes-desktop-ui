76911bc1f0f70f5a8855e3ae588d8b6b9d312b20
shruti misal <shrutimisal109@gmail.com>
2025-04-05 13:59:13 +0530

    leads

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Navbar/>
+       <Navbar/>
         {children}
       </body>
     </html>
